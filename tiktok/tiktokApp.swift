29e142fe3a3fc8bf7bf128b92f9da21cb16e01f7
wangjian <15995249412@163.com>
2024-08-09 13:24:16 +0800

    Initial Commit

---
+++ tiktok/tiktokApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktokApp.swift
+//  tiktok
+//
+//  Created by 炜季 on 2024/8/9.
+//
+
+import SwiftUI
+
+@main
+struct tiktokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
